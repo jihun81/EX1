c3de87cefb25814a174264af7104dbeb77c00c1a
zero777 <nonight0@naver.com>
2022-08-17 10:14:47 +0900

    테스트

---
+++ src/main/java/test/Member.java
@@ -0,0 +1,14 @@
+package test;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+public class Member {
+
+    @Id
+    private Long id;
+    private String name;
+
+}
---
+++ src/main/java/test/jpaMain.java
@@ -0,0 +1,11 @@
+package test;
+
+public class jpaMain {
+
+    public static void main(String[] args){
+
+
+    }
+
+
+}
